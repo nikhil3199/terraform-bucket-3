f002ae87248d933396b0394a21a5d677d910ce15
Nikhil Phadke <60593847+nikhil3199@users.noreply.github.com>
2024-01-11 16:29:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,5 +3,5 @@
 variable "project_id" {
   description = "The ID of the project in which to provision resources."
   type        = string
-  default     = "steam-treat-288817"
+  default     = "refined-density-410807"
 }
